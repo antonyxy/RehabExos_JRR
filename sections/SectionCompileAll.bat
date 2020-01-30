3ebc4592c7399e414fcbe05cf364464300b3abd7
antony <a.frisoli@sssup.it>
2020-01-30 19:13:13 +0100

    All files initiated

---
+++ sections/SectionCompileAll.bat
@@ -0,0 +1,21 @@
+perl latexdiff.pl  00_abstract_old.tex  00_abstract.tex > 00_abstract_diff.tex
+perl latexdiff.pl 01_introduction_old.tex 01_introduction.tex > 01_introduction_diff.tex
+
+perl latexdiff.pl  02_system_design_old.tex  02_system_design.tex > 02_system_design_diff.tex
+
+
+perl latexdiff.pl  02_system_design_old.tex  02_system_design.tex > 02_system_design_diff.tex
+
+ 
+perl latexdiff.pl 03_dynamic_model_old.tex 03_dynamic_model.tex > 03_dynamic_model_diff.tex
+
+ 
+perl latexdiff.pl  04_controllers_old.tex 04_controllers.tex > 04_controllers_diff.tex
+
+perl latexdiff.pl  05_experiments_results_old.tex 05_experiments_results.tex > 05_experiments_results_diff.tex
+
+perl latexdiff.pl   06_discussion_old.tex  06_discussion.tex  > 06_discussion_diff.tex
+
+perl latexdiff.pl    07_conclusion_old.tex  07_conclusion.tex > 07_conclusion_diff.tex
+
+perl latexdiff.pl  08_appendix_old.tex 08_appendix.tex > 08_appendix_diff.tex
